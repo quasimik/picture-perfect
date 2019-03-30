f4664989c0be816916ea9025ca721538198697e8
Michael Liu <mike666234@gmail.com>
2019-03-30 15:51:32 -0700

    Add models

--- backend/polls/models.py
+++ backend/polls/models.py
@@ -4,6 +4,32 @@ from django.db import models
 from django.utils import timezone
 
 
+class Word(models.Model):
+    word = models.CharField(max_length=200)
+    freq = models.FloatField()
+    length = models.IntegerField()
+
+    def __str__(self):
+        return ("%s : %s" % (self.word, str(self.freq)))
+
+class Game(models.Model):
+    target = models.ForeignKey(Word, on_delete=models.PROTECT)
+    master = models.ForeignKey(User, on_delete=models.CASCADE)
+    invite = models.CharField(max_length=200) # three 4-letter words?
+    created_date = models.DateTimeField(auto_now_add=True)
+    timeout = models.DurationField(default=timedelta(minutes=2))
+
+    def seconds_remaining(self):
+        return self.created_date + self.timeout - timezone.now()
+
+class Team(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    word = models.ForeignKey(Word, on_delete=models.PROTECT)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
