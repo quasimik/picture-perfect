537ddb96d7a940fa0aa35632a7146c15c50bbf96
Henry Bui <henrytbui@hotmail.com>
2019-03-30 18:11:47 -0700

    Update TeamJoin

--- frontend/src/components/TeamJoin.js
+++ frontend/src/components/TeamJoin.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Button, Card, List, Image } from 'semantic-ui-react';
+import { Button, Card, List, Segment } from 'semantic-ui-react';
 
 class Member {
     constructor(key, name) {
@@ -13,15 +13,16 @@ class Member {
 class TeamJoin extends Component {    
 
     state = {
-        teamList: [
-            { name: "Team 1", members: [ new Member(0, "Jon"), new Member(1, "John"), new Member(2, "Juan"), new Member(3, "Justin") ] },
-            { name: "Team 2", members: [ new Member(4, "Alice"), new Member(5, "Bob"), new Member(6, "Eve"), new Member(7, "Mallory"), new Member(8, "Trent")]}
-        ]
+        teamList: {
+            "Team 1" : [ new Member(0, "Jon"), new Member(1, "John"), new Member(2, "Juan"), new Member(3, "Justin") ],
+            "Team 2" : [ new Member(4, "Alice"), new Member(5, "Bob"), new Member(6, "Eve"), new Member(7, "Mallory"), new Member(8, "Trent")]
+        },
+        time: 500
     }
 
     joinTeam = (event, props) => {
         let teams = this.state.teamList[props.teamName];
-        teams = teams.concat(new Member(this.props.playerKey, this.props.playerName));
+        teams.append(new Member(this.props.playerKey, this.props.playerName));
         const newState = {
             teamList: teams
         }
@@ -37,11 +38,14 @@ class TeamJoin extends Component {
         const playerKey = this.props.playerKey;
         const playerName = this.props.playerName;
 
-        const boxes = teams.map( (team) => <TeamJoinBox teamName={team.name} memberList={team.members} joinTeam={this.joinTeam}/> );
+        const boxes = (Object.entries(teams)).map( ([team, members]) => <TeamJoinBox teamName={team} memberList={members} joinTeam={this.joinTeam}/> );
         return (
             <div>
-                <h2>Hello {playerName} ({playerKey}), join a team!</h2>
-                {boxes}
+                <h3>Time Remaining: {this.state.time}</h3>
+                <Segment>
+                    <h2>Hello {playerName} ({playerKey}), join a team!</h2>
+                    <div>{boxes}</div>
+                 </Segment>
             </div>
         );
     }
